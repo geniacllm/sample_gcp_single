766605fcbae23f733be1ed603f84f77363251913
exMemory <11851443+exMemory@users.noreply.github.com>
2024-04-11 22:38:38 +0900

    Update and rename init2.sh to train_batch.sh

--- train_batch.sh
+++ train_batch.sh
@@ -2,14 +2,15 @@
 
 # Command line options go here
 #SBATCH --partition=g2
-#SBATCH --time=00:01:00
+#SBATCH --time=02:00:00
 #SBATCH --nodes=1
-#SBATCH --job-name=example
-#SBATCH --output=example.out
-#SBATCH --gpus-per-node=1
+#SBATCH --job-name=moetre
+#SBATCH --output=moetre.out
+#SBATCH --gpus-per-node=4
 
 # インストールしたcondaを有効化。
 source ~/miniconda3/etc/profile.d/conda.sh
 which conda && echo "====" && conda --version
 conda activate .venv
 
+bash ${HOME}/git/sample_gcp_single/script/pretrain_moe.sh
