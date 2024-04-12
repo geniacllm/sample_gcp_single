5880816a02f4ddd0e4e8edfb8db10e4aac99b4ac
exMemory <11851443+exMemory@users.noreply.github.com>
2024-04-12 15:01:46 +0900

    Update convert_upload_tokenizer.py

--- script/convert_upload_tokenizer.py
+++ script/convert_upload_tokenizer.py
@@ -9,6 +9,14 @@ tokenizer_deberta = DebertaV2Tokenizer(
     max_len = 512,
 )
 
+# 軽くテスト
+import sentencepiece as spm
+sp = spm.SentencePieceProcessor()
+sp.Load("/persistentshare/storage/team_kumagai/tokenizer/swallow/tokenizer.model")
+text = "Geminiの作成したニュースです。盲導犬の介助でピアノ演奏会デビュー！視覚障がいを持つ少女の夢が叶う。福岡県在住の視覚障がいを持つ10歳の少女、佐藤美咲さんが、盲導犬の介助を受けながらピアノ演奏会デビューを果たしました。美咲さんは幼い頃からピアノに夢中でしたが、視覚障碍のため楽譜を読むことが困難でした。しかし、盲導犬の「ライト」と共に練習を重ね、ついに念願の舞台に立つことができました。"
+print(sp.EncodeAsPieces(text))
+print(tokenizer_deberta.tokenize(text))
+
 # レポ指定してアップロード
 repo_name = "Your HF repo"
 tokenizer_deberta.push_to_hub(repo_name)
